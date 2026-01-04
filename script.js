a2a5e0ef6bef4d89231aa045da3b60b3965d4d61
alinegracia19 <251604976+alinegracia19@users.noreply.github.com>
2026-01-05 01:59:23 +0700

    Fix script.js, so the user can only play the game 5 rounds per section

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const playerChoiceBtn = document.querySelectorAll(".player-choice");
 const playerScoreText = document.getElementById("player-score");
 const computerScoreText = document.getElementById("computer-score");
 const description = document.getElementById("description");
+const btnContainer = document.querySelector(".btn");
+const yourChoiceText = document.querySelector("h2");
 let playerScore = 0;
 let computerScore = 0;
+let totalRound = 1;
 
 const choices = ["rock", "paper", "scissors"];
 
@@ -13,22 +16,33 @@ function getComputerChoice () {
 
 function playRound (humanChoice) {
     const computerChoice = getComputerChoice();
-    if (humanChoice === computerChoice) {
+    if (totalRound < 5) {
+        if (humanChoice === computerChoice) {
         playerScore++;
         computerScore++;
         description.textContent = `It's a tie!! Both you and the computer chose ${humanChoice}`;
-        playerScoreText.textContent = playerScore;
-        computerScoreText.textContent = computerScore;
     } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
         playerScore++;
         description.textContent = `You win!! You chose ${humanChoice} and the computer chose ${computerChoice}`;
-        playerScoreText.textContent = playerScore;
-        computerScoreText.textContent = computerScore;
     } else {
         computerScore++;
         description.textContent = `Computer Win!! The computer chose ${computerChoice} and you chose ${humanChoice}`;
-        playerScoreText.textContent = playerScore;
-        computerScoreText.textContent = computerScore;
+        
+    }
+    totalRound++;
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+    }
+    else {
+        btnContainer.style.display = "none";
+        yourChoiceText.style.display = "none";
+        if (playerScore > computerScore) {
+            description.textContent = `YOU WIN!! Your score is ${playerScore} and the computer score is ${computerScore}`;
+        } else if (playerScore < computerScore) {
+            description.textContent = `YOU LOSE!! Your score is ${playerScore} and the computer score i ${computerScore}`;
+        } else {
+            description.textContent = `IT'S A TIE!! Both you and the computer score are ${playerScore}`;
+        }
     }
 }
 
